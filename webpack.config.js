d309d09cb8c4cd82317e783f428e9d0448d80c84
Tahir Civan <69795597+tahircivann@users.noreply.github.com>
2025-07-02 11:01:57 +0200

    Add webpack configuration file for module bundling and optimization

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  optimization: {
+    minimize: true
+  },
+  devtool: 'source-map'
+}; 
